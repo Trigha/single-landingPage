8716b56cf29043326c99e87141ceee882246553d
Trigha <adhindyatrigha@gmail.com>
2022-06-19 23:54:36 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import About from './component/about/about.jsx';
 import Pricing from './component/pricing/pricing.jsx';
 import Footer from './component/footer/footer.jsx';
 import './App.css';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    Aos.init({
+      duration: 2000
+    })
+  } , [])
+    
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <About />
-      <Pricing />
-      <Footer />
+      <Home data-aos="fade-up" />
+      <About data-aos="fade-up"/>
+      <Pricing data-aos="fade-up"/>
+      <Footer data-aos="fade-up"/>
     </div>
   );
 }
